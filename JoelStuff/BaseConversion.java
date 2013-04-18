10f25cc39adc3bd0560e4936c755fcb7ece51afd
Joel Zapata <joel.e.zapata@gmail.com>
2013-04-17 21:03:04 -0500

    Added comments

--- JoelStuff/BaseConversion.java
+++ JoelStuff/BaseConversion.java
@@ -1,5 +1,5 @@
 
-import java.util.Scanner;											//imports scanner class
+import java.util.Scanner;														//imports scanner class
 
 
 public class BaseConversion
@@ -9,17 +9,18 @@ public class BaseConversion
         System.out.println("Welcome to Base Conversion");
         System.out.println("Please enter your base (2,10, or 16)");
         Scanner scan = new Scanner(System.in);
-        int basein = scan.nextInt();									// Base is entered here
+        int basein = scan.nextInt();											// Base is entered here
         System.out.println("Your base is "+basein);
         System.out.println("Please enter your number");
-		int number = scan.nextInt();								// Number to be converted is entered here
+		int number = scan.nextInt();											// Number to be converted is entered here
 		System.out.println("Your number is "+number);
 		System.out.println("Please enter your desired base (2,10, or 16)");
-		int baseout = scan.nextInt();
+		int baseout = scan.nextInt();											// The desired base is this
+		System.out.println("Your desired base is "+baseout);
 		
 		// Start of conversion
 		if (baseout == 2)
-			Integer.toBinaryString(number);
+			Integer.toBinaryString(number);										// Changes number to bianary string representation (doesn't work)
 			System.out.println("Your new base 2 number is:" +number);
 		
     }
---
+++ JoelStuff/ConversionClass.java
@@ -0,0 +1,24 @@
+/**
+ * @(#)ConversionClass.java
+ *
+ *
+ * @author Jay-Z
+ * @version 1.00 2013/4/17
+ */
+
+
+public class ConversionClass
+{
+
+    public ConversionClass()
+    {
+		public void convertBinary(int i) 
+		{
+		 Integer.toBinaryString(int i);
+		}
+
+
+    }
+    
+    
+}
