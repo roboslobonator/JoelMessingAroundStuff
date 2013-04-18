57836300783fa5be35a0ec871ada1b9562cc3c95
Joel Zapata <joel.e.zapata@gmail.com>
2013-04-17 19:15:46 -0500

    It is wrong
    I don't know what I'm doing wrong

---
+++ JoelStuff/Base Conversion.java
@@ -0,0 +1,11 @@
+
+import.java.util;
+
+	public static void main(String [ ] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome to Base Conversion");
+		System.out.println("Please enter base");;
+		int base = input.nextLine();                        // Base is entered here
+		
+	}
