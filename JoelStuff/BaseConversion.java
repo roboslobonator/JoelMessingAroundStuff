5516ae05dd2a8bce4ec13573d3c0a0aeb18f3c1d
roboslobonator <roboslobonator@gmail.com>
2013-04-18 12:31:48 -0500

    Update BaseConversion.java

--- JoelStuff/BaseConversion.java
+++ JoelStuff/BaseConversion.java
@@ -1,27 +1,59 @@
-
 import java.util.Scanner;														//imports scanner class
 
 
-public class BaseConversion
+class BaseConverter
 {
-    public static void main(String [ ] args)
-    {
-        System.out.println("Welcome to Base Conversion");
-        System.out.println("Please enter your base (2,10, or 16)");
-        Scanner scan = new Scanner(System.in);
-        int basein = scan.nextInt();											// Base is entered here
-        System.out.println("Your base is "+basein);
-        System.out.println("Please enter your number");
-		int number = scan.nextInt();											// Number to be converted is entered here
-		System.out.println("Your number is "+number);
-		System.out.println("Please enter your desired base (2,10, or 16)");
-		int baseout = scan.nextInt();											// The desired base is this
-		System.out.println("Your desired base is "+baseout);
+	public static String fromHexToBin(String hex)
+	{
+		String bin = "";
+		String binFragment = "";
+		int iHex;
+		hex = hex.trim();
+		hex = hex.replaceFirst("0x", "");
 		
-		// Start of conversion
-		if (baseout == 2)
-			Integer.toBinaryString(number);										// Changes number to bianary string representation (doesn't work)
-			System.out.println("Your new base 2 number is:" +number);
-		
-    }
+		for(int i = 0; i < hex.length(); i++){
+			iHex = Integer.parseInt(""+hex.charAt(i),16);
+			binFragment = Integer.toBinaryString(iHex);
+			
+			while(binFragment.length() < 4){
+				binFragment = "0" + binFragment;
+				}
+				bin += binFragment;
+	      }
+	      return bin;
+	        		    	    	    	    	    		    	    
+	        }
+	        
+	            public static int fromBinToDec(String bin)
+	                {
+	                	    	double decNum = 0;
+	                	    			for (int i=0; i<bin.length(); i++){
+	                	    				        	if (bin.charAt(i) == '1'){
+	                	    				        		        		decNum=decNum+ Math.pow(2,bin.length()-1-i);
+	                	    				        	}
+	                	    			}
+	                	    				    	
+	                	    				    	    	return (int)decNum;    	
+	                }
+	                
+	                    
+	                        public static String fromBinToHex(String binNum)
+	                            {
+	                            	    	String hexNum = "";
+	                            	    	    	
+	                            	    	    	    	
+	                            	    	    	    	    	
+	                            	    	    	    	    	    	
+	                            	    	    	    	    	    	    	return hexNum;
+	                            }    
+	                                
+	                                    public static String fromDecToAny(int decNum, int desiredBase)
+	                                        {
+	                                        	    	String newNum = "";
+	                                        	    	    	
+	                                        	    	    	
+	                                        	    	    	    	
+	                                        	    	    	    	    	
+	                                        	    	    	    	    	    	return newNum;       	
+	                                        }
 }
